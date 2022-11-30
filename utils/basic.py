189a938305c4360bd871de095cc41ea76d7c7115
diddone <dmpozdeev@edu.hse.ru>
2022-11-30 14:18:18 +0300

    Add utils

---
+++ utils/basic.py
@@ -0,0 +1,8 @@
+import torch
+import numpy as np
+
+def set_random_seed(seed):
+    torch.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    np.random.seed(seed)
